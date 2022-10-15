722bf988351afa10bc6c2989dcca8956e23e5bd5
Lilian <73485512+LysnakeIT@users.noreply.github.com>
2022-10-15 13:16:30 +0000

    📝Ajout d'un nouveau parcours

--- src/components/ParcoursSection.vue
+++ src/components/ParcoursSection.vue
@@ -105,15 +105,15 @@ export default {
                 <div :class="parcoursActiveProfessionnel" v-show="parcoursType === 'professionel'">
                     <div class="parcours__data">
                         <div>
-                            <h3 class="dark:text-ternary-light parcours__title">Développeur Front-End</h3>
+                            <h3 class="dark:text-ternary-light parcours__title">Apprenti Digital Compliance & Cybersécurité</h3>
                             <span class="dark:text-ternary-light parcours__subtitle">
-                                <p style="margin : 0 !important;">TAC ECONOMICS</p>
+                                <p style="margin : 0 !important;">SANOFI</p>
                             </span>
                             <div class="parcours__calendar">
                                 <svg width="1em" xmlns="http://www.w3.org/2000/svg" class="ui-svg-inline" fill="currentColor" data-name="Layer 1" viewBox="0 0 24 24">
                                     <path d="M12,19a1,1,0,1,0-1-1A1,1,0,0,0,12,19Zm5,0a1,1,0,1,0-1-1A1,1,0,0,0,17,19Zm0-4a1,1,0,1,0-1-1A1,1,0,0,0,17,15Zm-5,0a1,1,0,1,0-1-1A1,1,0,0,0,12,15ZM19,3H18V2a1,1,0,0,0-2,0V3H8V2A1,1,0,0,0,6,2V3H5A3,3,0,0,0,2,6V20a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V6A3,3,0,0,0,19,3Zm1,17a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V11H20ZM20,9H4V6A1,1,0,0,1,5,5H6V6A1,1,0,0,0,8,6V5h8V6a1,1,0,0,0,2,0V5h1a1,1,0,0,1,1,1ZM7,15a1,1,0,1,0-1-1A1,1,0,0,0,7,15Zm0,4a1,1,0,1,0-1-1A1,1,0,0,0,7,19Z"/>
                                 </svg>
-                                Avril, 2022 - Juin, 2022
+                                Septembre, 2022 - En cours
                             </div>
                         </div>
                         <div>
@@ -129,6 +129,21 @@ export default {
                             <span class="parcours__line"></span>
                         </div>
                         <div>
+                            <h3 class="dark:text-ternary-light parcours__title">Développeur Front-End</h3>
+                            <span class="dark:text-ternary-light parcours__subtitle">
+                                <p style="margin : 0 !important;">TAC ECONOMICS</p>
+                            </span>
+                            <div class="parcours__calendar">
+                                <svg width="1em" xmlns="http://www.w3.org/2000/svg" class="ui-svg-inline" fill="currentColor" data-name="Layer 1" viewBox="0 0 24 24">
+                                    <path d="M12,19a1,1,0,1,0-1-1A1,1,0,0,0,12,19Zm5,0a1,1,0,1,0-1-1A1,1,0,0,0,17,19Zm0-4a1,1,0,1,0-1-1A1,1,0,0,0,17,15Zm-5,0a1,1,0,1,0-1-1A1,1,0,0,0,12,15ZM19,3H18V2a1,1,0,0,0-2,0V3H8V2A1,1,0,0,0,6,2V3H5A3,3,0,0,0,2,6V20a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V6A3,3,0,0,0,19,3Zm1,17a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V11H20ZM20,9H4V6A1,1,0,0,1,5,5H6V6A1,1,0,0,0,8,6V5h8V6a1,1,0,0,0,2,0V5h1a1,1,0,0,1,1,1ZM7,15a1,1,0,1,0-1-1A1,1,0,0,0,7,15Zm0,4a1,1,0,1,0-1-1A1,1,0,0,0,7,19Z"/>
+                                </svg>
+                                Avril, 2022 - Juin, 2022
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="parcours__data">
+                        <div>
                             <h3 class="dark:text-ternary-light parcours__title">Agent de fabrication</h3>
                             <span class="dark:text-ternary-light parcours__subtitle">
                                 <p style="margin : 0 !important;">SIKA</p>
@@ -137,12 +152,21 @@ export default {
                                 <svg width="1em" xmlns="http://www.w3.org/2000/svg" class="ui-svg-inline" fill="currentColor" data-name="Layer 1" viewBox="0 0 24 24">
                                     <path d="M12,19a1,1,0,1,0-1-1A1,1,0,0,0,12,19Zm5,0a1,1,0,1,0-1-1A1,1,0,0,0,17,19Zm0-4a1,1,0,1,0-1-1A1,1,0,0,0,17,15Zm-5,0a1,1,0,1,0-1-1A1,1,0,0,0,12,15ZM19,3H18V2a1,1,0,0,0-2,0V3H8V2A1,1,0,0,0,6,2V3H5A3,3,0,0,0,2,6V20a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V6A3,3,0,0,0,19,3Zm1,17a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V11H20ZM20,9H4V6A1,1,0,0,1,5,5H6V6A1,1,0,0,0,8,6V5h8V6a1,1,0,0,0,2,0V5h1a1,1,0,0,1,1,1ZM7,15a1,1,0,1,0-1-1A1,1,0,0,0,7,15Zm0,4a1,1,0,1,0-1-1A1,1,0,0,0,7,19Z"/>
                                 </svg>
-                                Juillet, 2021 - Juillet, 2021
+                               Juillet, 2021 - Juillet, 2021
                             </div>
+                        </div>
+                        <div>
+                            <span class="parcours__rounder"></span>
+                            <span class="parcours__line"></span>
                         </div>
                     </div>
 
                     <div class="parcours__data">
+                        <div></div>
+                        <div>
+                            <span class="parcours__rounder"></span>
+                            <span class="parcours__line"></span>
+                        </div>
                         <div>
                             <h3 class="dark:text-ternary-light parcours__title">ASTEP</h3>
                             <span class="dark:text-ternary-light parcours__subtitle">
@@ -155,17 +179,9 @@ export default {
                                 Février, 2021 - Avril, 2021
                             </div>
                         </div>
-                        <div>
-                            <span class="parcours__rounder"></span>
-                            <span class="parcours__line"></span>
-                        </div>
                     </div>
 
                     <div class="parcours__data">
-                        <div></div>
-                        <div>
-                            <span class="parcours__rounder"></span>
-                        </div>
                         <div>
                             <h3 class="dark:text-ternary-light parcours__title">Stage développement web 3ème</h3>
                             <span class="dark:text-ternary-light parcours__subtitle">
@@ -177,6 +193,9 @@ export default {
                                 </svg>
                                 Janvier, 2017 - Janvier, 2017
                             </div>
+                        </div>
+                        <div>
+                            <span class="parcours__rounder"></span>
                         </div>
                     </div>
                 </div>
