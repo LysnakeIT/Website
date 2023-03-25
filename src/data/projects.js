0eff699b6d45e4616483539558da2a4d5c8d3593
Lilian <73485512+LysnakeIT@users.noreply.github.com>
2023-03-25 16:49:07 +0100

    ✨Routage des projets vers leurs repo github

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,41 +2,49 @@ const listeDesProjets = [
 	{
 		title: 'Kroma-bot',
 		lang : 'JavaScript',
+		link: 'https://github.com/LysnakeIT/Kroma-bot',
 		img: require('@/assets/images/kromabot.webp'),
 	},
 	{
 		title: 'Les batisseurs du Moyen-Age',
 		lang : 'Java',
+		link: 'https://github.com/LysnakeIT/Les-Batisseurs-Moyen-Age',
 		img: require('@/assets/images/batisseurs.webp'),
 	},
 	{
 		title: 'SportTrack',
 		lang : 'PHP',
+		link: 'https://github.com/LysnakeIT/SportTrack',
 		img: require('@/assets/images/sporttrack.webp'),
 	},
 	{
 		title: 'Convertisseur',
 		lang : 'JavaScript',
+		link: 'https://github.com/LysnakeIT/Convertisseur',
 		img: require('@/assets/images/convertisseur.webp'),
 	},
 	{
 		title: 'Todo App',
 		lang : 'Java',
+		link: 'https://github.com/LysnakeIT/Todo-App',
 		img: require('@/assets/images/todoapp.webp'),
 	},
 	{
 		title: 'Weather App',
 		lang : 'Vue.JS',
+		link: 'https://github.com/LysnakeIT/WeatherApp',
 		img: require('@/assets/images/meteo.webp'),
 	},
 	{
 		title: 'Chat-custom-twitch',
 		lang : 'Vue.JS',
+		link: 'https://github.com/LysnakeIT/Chat-custom-twitch',
 		img: require('@/assets/images/chat-custom.webp'),
 	},
 	{
 		title: 'Blind Quiz',
 		lang : 'Python',
+		link: 'https://github.com/LysnakeIT/Blind-Quiz',
 		img: require('@/assets/images/blindquiz.webp'),
 	}
 ];
