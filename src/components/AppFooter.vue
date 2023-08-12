f05bde3427e49e63d0d6c119b776e48288c51e12
Lilian <73485512+LysnakeIT@users.noreply.github.com>
2023-08-12 18:00:37 +0200

    🎨Changement css du footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -12,7 +12,7 @@ export default {
 
 <template>
 	<div class="container mx-auto ">
-		<div class="pt-20 sm:pt-30 pb-8 mt-20 border-t-2 border-primary-light dark:border-secondary-dark">
+		<div class="pt-12 sm:pt-30 pb-8 mt-20 border-t-2 border-primary-light dark:border-secondary-dark">
 
 			<div class="flex justify-center items-center text-center">
 				<div class="font-general-regular text-lg text-ternary-dark dark:text-ternary-light"> &copy;{{ copyrightDate }}
